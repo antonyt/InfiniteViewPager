6d004ec5c4358f4759971eb13dacac9a512d0b2c
Antony Tran <antonyt@gmail.com>
2012-03-11 16:44:36 +0000

    Initial commit.

---
+++ src/com/antonyt/infiniteviewpager/ColourFragment.java
@@ -0,0 +1,39 @@
+package com.antonyt.infiniteviewpager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Example Fragment class that shows an identifier inside a TextView.
+ * 
+ */
+public class ColourFragment extends Fragment {
+
+    private int identifier;
+    private int colour;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	Bundle args = getArguments();
+	colour = args.getInt("colour");
+	identifier = args.getInt("identifier");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	TextView v = new TextView(getActivity());
+	v.setGravity(Gravity.CENTER);
+	v.setTextSize(40);
+	v.setTextColor(Color.BLACK);
+	v.setBackgroundColor(colour);
+	v.setText("Fragment ID: " + identifier);
+	return v;
+    }
+}
---
+++ src/com/antonyt/infiniteviewpager/InfinitePagerAdapter.java
@@ -0,0 +1,96 @@
+package com.antonyt.infiniteviewpager;
+
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * A PagerAdapter that wraps around another PagerAdapter to handle paging
+ * wrap-around.
+ * 
+ */
+public class InfinitePagerAdapter extends PagerAdapter {
+
+    private static final String TAG = "InfinitePagerAdapter";
+    private static final boolean DEBUG = true;
+
+    private PagerAdapter adapter;
+
+    public InfinitePagerAdapter(PagerAdapter adapter) {
+	this.adapter = adapter;
+    }
+
+    @Override
+    public int getCount() {
+	// warning: scrolling to very high values (1,000,000+) results in
+	// strange drawing behaviour
+	return Integer.MAX_VALUE;
+    }
+
+    /**
+     * @return the {@link #getCount()} result of the wrapped adapter
+     */
+    public int getRealCount() {
+	return adapter.getCount();
+    }
+
+    @Override
+    public Object instantiateItem(View container, int position) {
+	int virtualPosition = position % getRealCount();
+	debug("instantiateItem: real position: " + position);
+	debug("instantiateItem: virtual position: " + virtualPosition);
+
+	// only expose virtual position to the inner adapter
+	return adapter.instantiateItem(container, virtualPosition);
+    }
+
+    @Override
+    public void destroyItem(View container, int position, Object object) {
+	int virtualPosition = position % getRealCount();
+	debug("destroyItem: real position: " + position);
+	debug("destroyItem: virtual position: " + virtualPosition);
+
+	// only expose virtual position to the inner adapter
+	adapter.destroyItem(container, virtualPosition, object);
+    }
+
+    /*
+     * Delegate rest of methods directly to the inner adapter.
+     */
+
+    @Override
+    public void finishUpdate(View container) {
+	adapter.finishUpdate(container);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+	return adapter.isViewFromObject(view, object);
+    }
+
+    @Override
+    public void restoreState(Parcelable bundle, ClassLoader classLoader) {
+	adapter.restoreState(bundle, classLoader);
+    }
+
+    @Override
+    public Parcelable saveState() {
+	return adapter.saveState();
+    }
+
+    @Override
+    public void startUpdate(View container) {
+	adapter.startUpdate(container);
+    }
+
+    /*
+     * End delegation
+     */
+
+    private void debug(String message) {
+	if (DEBUG) {
+	    Log.d(TAG, message);
+	}
+    }
+}
