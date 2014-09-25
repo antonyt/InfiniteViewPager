b602ee4ad82b6feec0df8d8d00e1d2f1fefe77af
kongyang80 <kongyang80@gmail.com>
2014-09-24 20:41:43 -0500

    Created a wrapper for FragmentPagerAdapter to allow the infinite viewpager to work with FragmentPagerAdapter of size 1 or higher
    Signed-off-by: kongyang80 <kongyang80@gmail.com>

---
+++ demo/src/main/java/com/antonyt/infiniteviewpager/InfiniteViewPager2Activity.java
@@ -0,0 +1,56 @@
+package com.antonyt.infiniteviewpager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+import com.example.circularpageview.R;
+
+public class InfiniteViewPager2Activity extends FragmentActivity {
+
+    private static final String TAG = "InfinitePagerAdapter";
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            int[] colours = new int[]{Color.CYAN, Color.MAGENTA};
+
+            @Override
+            public int getCount() {
+                return colours.length;
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                Fragment fragment = new ColourFragment();
+                Bundle args = new Bundle();
+                args.putInt("colour", colours[position]);
+                args.putInt("identifier", position);
+                fragment.setArguments(args);
+                return fragment;
+            }
+            
+        };
+
+
+        // wrap pager to provide a minimum of 4 pages
+    	MinFragmentPagerAdapter wrappedMinAdapter = new MinFragmentPagerAdapter(getSupportFragmentManager());
+    	wrappedMinAdapter.setAdapter(adapter);
+    	
+        // wrap pager to provide infinite paging with wrap-around
+        PagerAdapter wrappedAdapter = new InfinitePagerAdapter(wrappedMinAdapter);
+
+        // actually an InfiniteViewPager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(wrappedAdapter);
+
+    }
+}
